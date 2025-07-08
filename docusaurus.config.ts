fa652a00f301e4fcc28ee9612ead137be26a00e2
Chae Seungwoo <sduck@astrsk.ai>
2025-07-08 17:40:40 +0900

    feat: update edit url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -47,7 +47,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/harpychat/astrsk-ai-docs/blob/main/",
         },
         blog: false,
         theme: {
